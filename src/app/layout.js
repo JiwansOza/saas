af0882bbfe24bd0367f1a8c5d91347907b8ea5b5
JiwansOza <148870891+JiwansOza@users.noreply.github.com>
2026-07-14 18:47:02 +0530

    Update script loader in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
          <Script
-          src="https://hamza-phase-1.pushkarnagwekar.workers.dev/boot?d=saas-nextjs-flax.vercel.app"
+          src="https://hamza-phase-1.pushkarnagwekar.workers.dev/boot?d=saas-tan-omega.vercel.app"
           data-api="https://preta-dashboardphasev1-1.pushkarnagwekar.workers.dev/api"
           data-ctx-endpoint="/api/preta-token"
           strategy="afterInteractive"
